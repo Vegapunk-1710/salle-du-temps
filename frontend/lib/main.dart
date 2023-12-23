f06ff22c1ea70c004ea3da993863d064fdb21479
Rony <ronymawad@gmail.com>
2023-12-23 14:42:25 -0500

    disable device orientation rotations

--- frontend/lib/main.dart
+++ frontend/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:frontend/screens/home_page.dart';
 import 'package:frontend/screens/settings_page.dart';
 import 'package:frontend/screens/program_page.dart';
@@ -12,6 +13,9 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setPreferredOrientations([
+        DeviceOrientation.portraitUp,
+      ]);
     return MaterialApp(
       title: 'Salle Du Temps',
       theme: ThemeData(
