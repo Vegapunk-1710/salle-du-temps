e684888a483de57aeea4271f21e65240b70a776b
Rony <ronymawad@gmail.com>
2023-12-24 13:14:38 -0500

    add/remove progression on page only

--- frontend/lib/screens/exercise_page.dart
+++ frontend/lib/screens/exercise_page.dart
@@ -12,6 +12,9 @@ class ExercisePage extends StatefulWidget {
 }
 
 class _ExercisePageState extends State<ExercisePage> {
+  TextEditingController progressionWeightController = TextEditingController();
+  TextEditingController progressionSetsController = TextEditingController();
+  TextEditingController progressionRepsController = TextEditingController();
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -53,19 +56,72 @@ class _ExercisePageState extends State<ExercisePage> {
                     style:
                         TextStyle(fontWeight: FontWeight.w600, fontSize: 22))),
             Card(
-              margin: EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
               child: Padding(
-                padding: const EdgeInsets.all(20),
-                child: Text(widget.exercise.tutorial,style:
-                          const TextStyle(fontWeight: FontWeight.w400, fontSize: 16))),
+                  padding: const EdgeInsets.all(20),
+                  child: Text(widget.exercise.tutorial,
+                      style: const TextStyle(
+                          fontWeight: FontWeight.w400, fontSize: 16))),
             ),
             const Padding(
                 padding: EdgeInsets.all(8.0),
-                child: Text("Progression",
+                child: Text("Personal Progression",
                     style:
                         TextStyle(fontWeight: FontWeight.w600, fontSize: 22))),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (widget.exercise.progression.isNotEmpty) {
+                          widget.exercise.progression.removeLast();
+                        }
+                      });
+                    },
+                    icon: const Icon(Icons.delete_forever)),
+                Padding(
+                  padding: EdgeInsets.fromLTRB(5, 12, 0, 5),
+                  child: Row(
+                    children: [
+                      ProgressionTextField(
+                        hint: 'lbs',
+                        controller: progressionWeightController,
+                      ),
+                      ProgressionTextField(
+                        hint: 'sets',
+                        controller: progressionSetsController,
+                      ),
+                      ProgressionTextField(
+                        hint: 'reps',
+                        controller: progressionRepsController,
+                      ),
+                    ],
+                  ),
+                ),
+                IconButton(
+                    onPressed: () {
+                      setState(() {
+                        if (progressionWeightController.text.isNotEmpty &&
+                            progressionSetsController.text.isNotEmpty &&
+                            progressionRepsController.text.isNotEmpty) {
+                          final date = DateTime.now();
+                          String formattedDate =
+                              date.toIso8601String().split('T').first;
+                          int weight =
+                              int.parse(progressionWeightController.text);
+                          int sets = int.parse(progressionSetsController.text);
+                          int reps = int.parse(progressionRepsController.text);
+                          widget.exercise.progression
+                              .add((formattedDate, weight, sets, reps));
+                        }
+                      });
+                    },
+                    icon: const Icon(Icons.add))
+              ],
+            ),
             Card(
-              margin: EdgeInsets.all(10),
+              margin: const EdgeInsets.all(10),
               child: ListView.builder(
                 shrinkWrap: true,
                 physics: const NeverScrollableScrollPhysics(),
@@ -75,13 +131,22 @@ class _ExercisePageState extends State<ExercisePage> {
                     padding: const EdgeInsets.all(5.0),
                     child: Column(
                       children: [
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Text(widget.exercise.progression[index].$1),
-                            const Icon(Icons.arrow_right),
-                            Text("${widget.exercise.progression[index].$2} lbs") 
-                          ],
+                        FittedBox(
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                            children: [
+                              Text(widget.exercise.progression[index].$1),
+                              const Icon(Icons.arrow_right),
+                              Text(
+                                  "${widget.exercise.progression[index].$2} max lbs"),
+                              const Icon(Icons.arrow_right),
+                              Text(
+                                  "${widget.exercise.progression[index].$3} max sets"),
+                              const Icon(Icons.arrow_right),
+                              Text(
+                                  "${widget.exercise.progression[index].$4} max reps")
+                            ],
+                          ),
                         ),
                       ],
                     ),
@@ -89,33 +154,9 @@ class _ExercisePageState extends State<ExercisePage> {
                 },
               ),
             ),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Padding(
-                  padding: const EdgeInsets.fromLTRB(50,12,5,5),
-                  child: SizedBox(
-                    height: MediaQuery.of(context).size.height/20,
-                    width: MediaQuery.of(context).size.width/2,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      inputFormatters: <TextInputFormatter>[
-                         FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
-                        FilteringTextInputFormatter.digitsOnly
-                      ],
-                      decoration: const InputDecoration(
-                        border: OutlineInputBorder(borderRadius: BorderRadius.all(Radius.circular(20))),
-                        hintText: 'New Weight (in lbs)',
-                      ),
-                    ),
-                  ),
-                ),
-                IconButton(onPressed: (){
-                }, icon: const Icon(Icons.add))
-              ],
-            ),
+            SizedBox(height: 100,)
           ]))),
-          floatingActionButton: Row(
+      floatingActionButton: Row(
         mainAxisAlignment: MainAxisAlignment.start,
         children: [
           Padding(
@@ -129,6 +170,37 @@ class _ExercisePageState extends State<ExercisePage> {
             ),
           ),
         ],
+      ),
+    );
+  }
+}
+
+class ProgressionTextField extends StatelessWidget {
+  final String hint;
+  final TextEditingController controller;
+  const ProgressionTextField(
+      {super.key, required this.hint, required this.controller});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: SizedBox(
+        height: MediaQuery.of(context).size.height / 20,
+        width: MediaQuery.of(context).size.width / 6,
+        child: TextField(
+          keyboardType: TextInputType.number,
+          inputFormatters: <TextInputFormatter>[
+            FilteringTextInputFormatter.allow(RegExp(r'[0-9]')),
+            FilteringTextInputFormatter.digitsOnly
+          ],
+          controller: controller,
+          decoration: InputDecoration(
+            border: const OutlineInputBorder(
+                borderRadius: BorderRadius.all(Radius.circular(20))),
+            hintText: hint,
+          ),
+        ),
       ),
     );
   }
