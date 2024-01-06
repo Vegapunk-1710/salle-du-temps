e214dcd57c64579f4643acfe2f44d5ae98ddd903
Rony <ronymawad@gmail.com>
2024-01-06 16:28:30 -0500

    finished body progression

--- frontend/lib/models/user_model.dart
+++ frontend/lib/models/user_model.dart
@@ -9,10 +9,9 @@ class User {
   num startingWeight;
   int height;
   List<String> workouts;
-  List<(DateTime date, List<String> imagesPaths, String weight)> progression;
 
-  User(
-      {required this.id,
+  User({
+      required this.id,
       required this.username,
       required this.password,
       required this.name,
@@ -21,11 +20,10 @@ class User {
       required this.updatedAt,
       required this.startingWeight,
       required this.height,
-      required this.workouts,
-      required this.progression});
+      required this.workouts});
 
   @override
   String toString() {
-    return "id : $id\nusername : $username \npassword : $password\nname : $name\ndob : $dob\ncreatedAt : $createdAt\nupdatedAt: $updatedAt\nstartingWeight: $startingWeight\nworkouts : $workouts\nprogression : $progression";
+    return "id : $id\nusername : $username \npassword : $password\nname : $name\ndob : $dob\ncreatedAt : $createdAt\nupdatedAt: $updatedAt\nstartingWeight: $startingWeight\nworkouts : $workouts\n";
   }
 }
--- frontend/lib/screens/body/body_prog_page.dart
+++ frontend/lib/screens/body/body_prog_page.dart
@@ -25,7 +25,7 @@ class _BodyProgPageState extends State<BodyProgPage> {
   }
 
   saveProgression() async {
-    final directory = await getApplicationDocumentsDirectory();
+    final directory = await getApplicationCacheDirectory();
     final file = File('${directory.path}/bodyProgData.json');
     if (await file.exists()) {
       file.writeAsStringSync('');
@@ -35,10 +35,13 @@ class _BodyProgPageState extends State<BodyProgPage> {
   }
 
   getProgressions() async {
-    final directory = await getApplicationDocumentsDirectory();
+    final directory = await getApplicationCacheDirectory();
     final file = File('${directory.path}/bodyProgData.json');
-    final contents = await file.readAsString();
+    if (!await file.exists()) {
+      await file.create();
+    }
     try {
+      final contents = await file.readAsString();
       Iterable p = json.decode(contents);
       setState(() {
         progressions = List<BodyProgression>.from(
@@ -48,6 +51,32 @@ class _BodyProgPageState extends State<BodyProgPage> {
       if (kDebugMode) {
         print("Body Progressions File is empty.");
       }
+    }
+  }
+
+  void deleteProgression() {
+    if(this.mounted){
+      setState(() {
+        BodyProgression deletedProg = progressions.removeLast();
+        saveProgression();
+        getProgressions();
+        var snackBar = SnackBar(
+            content: Text('Deleted Body Progression : ${deletedProg.createdAt.toIso8601String().split("T").first}'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                if(this.mounted){
+                  setState(() {
+                  progressions.add(deletedProg);
+                  saveProgression();
+                  getProgressions();
+                });
+                }
+              },
+            ),
+          );
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+      });
     }
   }
 
@@ -68,32 +97,41 @@ class _BodyProgPageState extends State<BodyProgPage> {
             shrinkWrap: true,
             gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                 crossAxisCount: 3),
-            itemBuilder: (_, index) => GestureDetector(
-              onTap: () {
-                Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                        builder: (context) =>
-                            BodyProgHero(progressions[index])));
-              },
-              child: Padding(
-                padding: const EdgeInsets.all(4.0),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.circular(8.0),
-                  child: Hero(
-                    tag: 'bodyproghero${progressions[index].id}',
-                    child: Image.file(File(progressions[index].imagesPaths[0]),
-                        errorBuilder: (context, error, stackTrace) {
-                      return SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: const Card(
-                              elevation: 10, child: Icon(Icons.image)));
-                    }, fit: BoxFit.cover),
-                  ),
-                ),
-              ),
-            ),
-            itemCount: progressions.length,
+            itemBuilder: (_, index) => index < progressions.length
+                ? GestureDetector(
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) =>
+                                  BodyProgHero(progressions[index])));
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(8.0),
+                        child: Hero(
+                          tag: 'bodyproghero${progressions[index].id}',
+                          child: Image.file(
+                              File(progressions[index].imagesPaths[0]),
+                              errorBuilder: (context, error, stackTrace) {
+                            return SizedBox(
+                                width: MediaQuery.of(context).size.width,
+                                child: const Card(
+                                    elevation: 10, child: Icon(Icons.image)));
+                          }, fit: BoxFit.cover),
+                        ),
+                      ),
+                    ),
+                  )
+                : progressions.isEmpty
+                    ? const SizedBox.shrink()
+                    : IconButton(
+                        onPressed: () {
+                          handleDelete(context);
+                        },
+                        icon: const Icon(Icons.delete_forever)),
+            itemCount: progressions.length + 1,
           ),
         ),
       ),
@@ -123,6 +161,29 @@ class _BodyProgPageState extends State<BodyProgPage> {
               child: const Icon(Icons.create),
             ),
           ),
+        ],
+      ),
+    );
+  }
+
+  Future<dynamic> handleDelete(BuildContext context) {
+    return showDialog(
+      context: context,
+      barrierDismissible: false,
+      builder: (_) => AlertDialog(
+        title: const Text("Delete this progression ?"),
+        content: const Text(
+            "Are you sure you want to delete the last progression ?"),
+        actions: [
+          TextButton(
+              onPressed: () => Navigator.pop(context, 'Cancel'),
+              child: const Text("No")),
+          TextButton(
+              onPressed: () {
+                deleteProgression();
+                Navigator.pop(context, 'OK');
+              },
+              child: const Text("Yes")),
         ],
       ),
     );
