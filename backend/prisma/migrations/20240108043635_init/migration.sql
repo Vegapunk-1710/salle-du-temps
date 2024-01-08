f7a03c79615ceda30a7286229de9eb2b7d487938
Rony <ronymawad@gmail.com>
2024-01-07 23:38:39 -0500

    seeded backend with exercises

---
+++ backend/prisma/migrations/20240108043635_init/migration.sql
@@ -0,0 +1,92 @@
+-- CreateEnum
+CREATE TYPE "Difficulty" AS ENUM ('Beginner', 'Intermediate', 'Advanced');
+
+-- CreateEnum
+CREATE TYPE "Days" AS ENUM ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday');
+
+-- CreateEnum
+CREATE TYPE "Type" AS ENUM ('Aerobic', 'Strength', 'Stretching', 'Balance');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" TEXT NOT NULL,
+    "username" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT NOT NULL,
+    "dob" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "startingWeight" DOUBLE PRECISION NOT NULL,
+    "height" INTEGER NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Workout" (
+    "id" TEXT NOT NULL,
+    "imageURL" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "difficulty" "Difficulty" NOT NULL,
+    "time" INTEGER NOT NULL,
+    "description" TEXT NOT NULL,
+
+    CONSTRAINT "Workout_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Exercise" (
+    "id" TEXT NOT NULL,
+    "imageURL" TEXT NOT NULL,
+    "createdBy" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "title" TEXT NOT NULL,
+    "difficulty" "Difficulty" NOT NULL,
+    "time" INTEGER NOT NULL,
+    "type" "Type" NOT NULL,
+    "tutorial" TEXT NOT NULL,
+    "setsreps" TEXT NOT NULL,
+
+    CONSTRAINT "Exercise_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "_UserToWorkout" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "_ExerciseToWorkout" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_UserToWorkout_AB_unique" ON "_UserToWorkout"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_UserToWorkout_B_index" ON "_UserToWorkout"("B");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ExerciseToWorkout_AB_unique" ON "_ExerciseToWorkout"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ExerciseToWorkout_B_index" ON "_ExerciseToWorkout"("B");
+
+-- AddForeignKey
+ALTER TABLE "_UserToWorkout" ADD CONSTRAINT "_UserToWorkout_A_fkey" FOREIGN KEY ("A") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_UserToWorkout" ADD CONSTRAINT "_UserToWorkout_B_fkey" FOREIGN KEY ("B") REFERENCES "Workout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToWorkout" ADD CONSTRAINT "_ExerciseToWorkout_A_fkey" FOREIGN KEY ("A") REFERENCES "Exercise"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_ExerciseToWorkout" ADD CONSTRAINT "_ExerciseToWorkout_B_fkey" FOREIGN KEY ("B") REFERENCES "Workout"("id") ON DELETE CASCADE ON UPDATE CASCADE;
