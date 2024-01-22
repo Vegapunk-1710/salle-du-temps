afd58eddd3433c151fa007645b66b784b5f17314
Rony <ronymawad@gmail.com>
2024-01-21 23:26:51 -0500

    Update body_hero_page.dart

--- frontend/lib/screens/body/body_hero_page.dart
+++ frontend/lib/screens/body/body_hero_page.dart
@@ -1,6 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:frontend/models/body_prog_model.dart';
 
 class BodyProgHero extends StatefulWidget {
@@ -16,71 +17,98 @@ class _BodyProgHeroState extends State<BodyProgHero> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Stack(
-        alignment: Alignment.bottomRight,
-        children: [
-            Hero(
-                tag: 'bodyproghero${widget.progression.id}',
-                child: PageView.builder(
-                  itemCount: widget.progression.imagesPaths.length,
-                  onPageChanged: (index) => setState(() {
-                    page_index = index;
-                  }),
-                  itemBuilder: (context, index) {
-                    return InteractiveViewer(
-                      child: Image.file(
-                        File(widget.progression.imagesPaths[index]),
-                        errorBuilder: (context, error, stackTrace) {
-                      return SizedBox(
-                          width: MediaQuery.of(context).size.width,
-                          child: const Card(
-                              elevation: 10, child: Icon(Icons.image)));
-                    },
-                          fit: BoxFit.cover),
-                    );
-                  },
-                )),
-            SafeArea(
-              child: Column(children: [
-                HeroText("${page_index+1}/${widget.progression.imagesPaths.length}", 25)
-              ],),
+      body: Stack(alignment: Alignment.bottomRight, children: [
+        Hero(
+            tag: 'bodyproghero${widget.progression.id}',
+            child: PageView.builder(
+              itemCount: widget.progression.imagesPaths.length,
+              onPageChanged: (index) => setState(() {
+                page_index = index;
+              }),
+              itemBuilder: (context, index) {
+                return InteractiveViewer(
+                  child: Image.file(File(widget.progression.imagesPaths[index]),
+                      errorBuilder: (context, error, stackTrace) {
+                    return SizedBox(
+                        width: MediaQuery.of(context).size.width,
+                        child: const Card(
+                            elevation: 10, child: Icon(Icons.image)));
+                  }, fit: BoxFit.cover),
+                );
+              },
+            )),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                HeroText(
+                    "${page_index + 1}/${widget.progression.imagesPaths.length}",
+                    25)
+              ],
             ),
-            SafeArea(
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                crossAxisAlignment: CrossAxisAlignment.end,
-                children: [
-                HeroText(widget.progression.createdAt.toIso8601String().split("T").first, 25),
-                HeroText("${widget.progression.currentWeight} lbs" , 25),
-              ],),
-            )
+          ),
+        ),
+        SafeArea(
+          child: Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              crossAxisAlignment: CrossAxisAlignment.end,
+              children: [
+                HeroText(
+                    widget.progression.createdAt
+                        .toIso8601String()
+                        .split("T")
+                        .first,
+                    25),
+                HeroText("${widget.progression.currentWeight} lbs", 25),
+              ],
+            ),
+          ),
+        )
       ]),
+  floatingActionButtonLocation: FloatingActionButtonLocation.startTop,
+      floatingActionButton: Padding(
+        padding: const EdgeInsets.all(8),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: [
+            FloatingActionButton(onPressed: () {
+              Navigator.of(context).pop();
+            },
+            child: const Icon(Icons.arrow_back),
+            
+            ),
+          ],
+        ),
+      ),
     );
   }
 
   Stack HeroText(String text, double? size) {
     return Stack(
-children: <Widget>[
-  Text(
-    text,
-    style: TextStyle(
-      fontSize: size,
-      fontWeight: FontWeight.w800,
-      foreground: Paint()
-        ..style = PaintingStyle.stroke
-        ..strokeWidth = 1
-        ..color = Colors.black,
-    ),
-  ),
-  Text(
-     text,
-    style: TextStyle(
-      fontSize: size,
-      fontWeight: FontWeight.w800,
-      color: Colors.grey[300],
-    ),
-  ),
-],
-);
+      children: <Widget>[
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: size,
+            fontWeight: FontWeight.w800,
+            foreground: Paint()
+              ..style = PaintingStyle.stroke
+              ..strokeWidth = 1
+              ..color = Colors.black,
+          ),
+        ),
+        Text(
+          text,
+          style: TextStyle(
+            fontSize: size,
+            fontWeight: FontWeight.w800,
+            color: Colors.white,
+          ),
+        ),
+      ],
+    );
   }
 }
